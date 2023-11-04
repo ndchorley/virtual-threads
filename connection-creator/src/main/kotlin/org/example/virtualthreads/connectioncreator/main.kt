4331353ed01c6207af01cf9acf13f9fafb75288d
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-11-04 20:31:18 +0000

    Create connections to the server in separate threads

--- connection-creator/src/main/kotlin/org/example/virtualthreads/connectioncreator/main.kt
+++ connection-creator/src/main/kotlin/org/example/virtualthreads/connectioncreator/main.kt
@@ -2,6 +2,9 @@ package org.example.virtualthreads.connectioncreator
 
 fun main() {
     (1 .. 10).forEach {
-        Client().sendMessageToServerAndDisplayResponse()
+        Thread.ofPlatform().start(::connectionCreatingTask)
     }
 }
+
+private fun connectionCreatingTask() =
+    Client().sendMessageToServerAndDisplayResponse()

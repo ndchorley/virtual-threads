ffc95adf2f546c68c1c59f5495a089474553db77
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-11-04 09:53:44 +0000

    Put reading code together in the client

--- client/src/main/kotlin/org/example/virtualthreads/client/Client.kt
+++ client/src/main/kotlin/org/example/virtualthreads/client/Client.kt
@@ -10,10 +10,9 @@ class Client {
         val socket = Socket("127.0.0.1", 8080)
 
         val writer = PrintWriter(socket.getOutputStream(), true)
-        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
-
         writer.println("Hello")
 
+        val reader = BufferedReader(InputStreamReader(socket.getInputStream()))
         println("Received: ${reader.readLine()}")
     }
 }

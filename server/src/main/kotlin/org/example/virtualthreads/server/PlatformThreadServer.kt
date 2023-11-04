217e841924a1bd16d79026fbc08ed9a17a2bd0c7
Nicky Chorley <1155941+ndchorley@users.noreply.github.com>
2023-11-04 10:20:35 +0000

    Add a blank line in PlatformThreadServer.kt

--- server/src/main/kotlin/org/example/virtualthreads/server/PlatformThreadServer.kt
+++ server/src/main/kotlin/org/example/virtualthreads/server/PlatformThreadServer.kt
@@ -6,6 +6,7 @@ import java.net.ServerSocket
 
 class PlatformThreadServer(val port: Int) {
     private val socket = ServerSocket(port)
+
     fun start() {
         println("Listening on port: $port")
 
